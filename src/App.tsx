50e64faa792031d1a1ff453517a5c08004dcd5cd
Davide Chigliaro <davide.chigliaro@grupposcai.it>
2025-06-05 15:03:06 +0200

    Feat: alert test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import Message from "./Message";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
-function App() {
+const App = () => {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      {" "}
-      <Message />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          Hello <b>World</b>
+        </Alert>
+      )}
+      <Button onclick={() => setAlertVisibility(true)} color="secondary">
+        Test
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
